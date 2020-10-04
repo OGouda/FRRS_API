b146e71ea39ee570583a14127e198257edee6c0f
oabuwaraga <72347329+oabuwaraga@users.noreply.github.com>
2020-10-04 18:30:08 +0400

    Update First_Setup.sh

--- API/First_Setup.sh
+++ API/First_Setup.sh
@@ -4,6 +4,5 @@ exec python manage.py db upgrade &
 exec python manage.py db init &
 exec python manage.py db upgrade &
 exec python manage.py db migrate &
-exec python add_new_API_table.py &
 #exec python app.py
 #exec "$@"
