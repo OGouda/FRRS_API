6033246cca37a24b60a5671e71a07eb400cd6749
oabuwaraga <72347329+oabuwaraga@users.noreply.github.com>
2020-10-04 18:29:28 +0400

    Update First.py

--- API/First.py
+++ API/First.py
@@ -4,7 +4,6 @@ try:
     os.system('python manage.py db upgrade')
     os.system('python manage.py db init')
     os.system('python manage.py db migrate')
-    os.system('python add_new_API_table.py')
 except:
     pass
 os.system('python app.py')
