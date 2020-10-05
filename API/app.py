933e250ba789befde1db2dd1d4b003edecb63e0e
oabuwaraga <72347329+oabuwaraga@users.noreply.github.com>
2020-10-05 18:57:39 +0400

    Update app.py

--- API/app.py
+++ API/app.py
@@ -551,5 +551,5 @@ api.add_resource(NewEvent, '/new-event')
 api.add_resource(EventName, '/event-name')
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000, debug=True)
+    app.run(host= '0.0.0.0', port=5000, debug=False)
 
